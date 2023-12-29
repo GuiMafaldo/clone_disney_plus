c9ed3dc2891313655c576145f8dae80d49db1f2e
Guilherme <thon_firmesao@hotmail.com>
2023-12-29 20:57:45 -0300

    criada o roda pé e o header e suas propriedades

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons  = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            exibeElementosDoHeader();
+        } else {
+            ocultaElementosDoHeader();
+        }
+    })
     
 
+    
+    
+    //Atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao){
             const abaAlvo = botao.target.dataset.tabButton;
@@ -15,11 +31,25 @@ document.addEventListener('DOMContentLoaded', function(){
             
         })
     }
-
+    // Seção FAQ, Accordion2
     for (let i = 0; i < questions.length; i++) {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 })
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+
+
+
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
